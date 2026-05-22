c140a78d4f24f8f2dc405d7edcfe091a3bb562d3
bharathautomation <139088966+bharathtechacademy@users.noreply.github.com>
2026-05-22 08:27:20 +0530

    new changes added by balaji

--- login.js
+++ login.js
@@ -3,3 +3,4 @@ console.log("Login Page - Line 2");
 console.log("Login Page - Line 3 - Added by Balaji");
 console.log("Login Page - Line 4 - Added By Bharath");
 console.log("Login Page - Line 5 - Added By Bharath");
+console.log("Login Page - Line 5 - Added By Balaji");
